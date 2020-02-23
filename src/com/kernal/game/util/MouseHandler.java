022adaf362cd6bf820cbf23944672e20c5654070
ZackeryGeorge <zackery.boss.george@gmail.com>
2020-02-23 21:47:29 +0200

    MouseHandler WORK IN PROGRESS.

--- src/com/kernal/game/util/MouseHandler.java
+++ src/com/kernal/game/util/MouseHandler.java
@@ -1,5 +1,53 @@
 package com.kernal.game.util;
 
-public class MouseHandler {
+import com.kernal.game.GamePanel;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class MouseHandler implements MouseListener, MouseMotionListener {
+
+    private static int mouseX = -1;
+    private static int mouseY = -1;
+    private static int mouseB = -1;
+
+    public MouseHandler(GamePanel game) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
